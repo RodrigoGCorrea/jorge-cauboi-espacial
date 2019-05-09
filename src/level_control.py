1d0d46f4fae3ef1cc1017e7983be3d0cc74ea850
Rodrigo Correa <rodrigogc@id.uff.br>
2019-05-09 19:40:12 -0300

    Game Level Commit

--- src/level_control.py
+++ src/level_control.py
@@ -1,10 +1,19 @@
+from PPlay.sprite import Sprite
 
 class LevelControl(object):
-    def __init__(self, level_mtx):
+    def __init__(self, window, level_mtx):
         self.level_mtx = level_mtx
-    
+        self.window = window
+        self.__draw()
+
     def run(self):
-        pass
+        self.__draw()
     
     def __draw(self):
-        pass
+        for col in range(len(self.level_mtx)):
+            for lin in range(len(self.level_mtx[0])):
+                if self.level_mtx[col][lin] == 1:
+                    tile = Sprite("src\\assets\\floor.png")
+                    tile.set_position(lin * tile.width, col * tile.height)
+                    tile.draw()
+       
--- src/main.py
+++ src/main.py
@@ -1,11 +1,30 @@
 from PPlay.window import Window
+from PPlay.sprite import Sprite
+from level_control import LevelControl
 import globals
 
 def main():
     window = Window(globals.WIDTH, globals.HEIGHT)
     window.set_title("JSC")
+    player = Sprite("src\\assets\\sprite_player clone-1.png.png")
+    background = Sprite("src\\assets\\maxresdefault.jpg")
+    lifebar = Sprite("src\\assets\\lifebar.png")
+    
+    player.set_position(window.width/2 - player.width/2, window.height - 50 - player.height)
+    background.set_position(0, -50)
+
+    lifebar.draw()
+    background.draw()
+    player.draw()
+    
+    map = LevelControl(window, globals.LEVEL1)
 
     while globals.GAME_STARTED:
+        background.draw()
+        lifebar.draw()
+        map.run()
+        player.draw()
+
         window.update()
 
 if __name__ == "__main__":
