2a1bbbad931ebb17b82c770b12a0e96b048b733a
Geraldo Fada <geraldofada.neto@gmail.com>
2019-05-17 22:37:18 -0300

    state and direction using dicts

--- src/entity.py
+++ src/entity.py
@@ -11,8 +11,16 @@ class Entity(object):
 		self.animation.set_total_duration(frames * globals.FRAME_SPEED)
 		self.animation.play()
 
-		self.state = "idle" 
-		self.direction = "right"
+		self.direction = {
+			"right": True,
+			"left": False
+		}
+		self.state = {
+			"idle": True,
+			"walking": False,
+			"jumping": False,
+			"colliding": False
+		}
 
 		self.dx = 0
 		self.dy = 0
@@ -25,14 +33,14 @@ class Entity(object):
 		self.lvl = 1
 
 	def update(self, dt, level):
-		if self.state == "walking":
-			if self.direction == "left":
+		if self.state["walking"]:
+			if self.direction["left"]:
 				self.dx = -globals.X_VELOCITY_PLAYER
-			elif self.direction == "right":
+			elif self.direction["right"]:
 				self.dx = globals.X_VELOCITY_PLAYER
-		elif self.state == "idle":
+		elif self.state["idle"]:
 			self.dx = 0
-		elif self.state == "jumping":
+		elif self.state["jumping"]:
 			self.dy = globals.Y_VELOCITY_PLAYER
 			self.jump(self.dy, dt)
 
@@ -40,23 +48,33 @@ class Entity(object):
 		self.set_behavior(level)
 	
 	def render(self):
-		if self.state == "idle":
+		if self.state["idle"]:
 			self.set_animation("./src/assets/jorge_idle.png", 8)
-			if self.direction == "left":
+			if self.direction["left"]:
 				self.animation.image = flip(self.animation.image, True, False)
-		elif self.state == "walking":
+		elif self.state["walking"]:
 			self.set_animation("./src/assets/jorge_running.png", 8)
-			if self.direction == "left":
+			if self.direction["left"]:
 				self.animation.image = flip(self.animation.image, True, False)
 
 		self.animation.update()
 		self.animation.draw()
+
+	def walk(self, dx):
+		self.animation.x += dx
+
+	def jump(self, dy, dt):
+		self.animation.y = self.y0 - dy * self.y_time - (globals.GRAVITY * (self.y_time)**2)/2
+		self.y_time += globals.FALL_TIME * dt
+	
+	def attack(self):
+		pass
 	
 	def set_behavior(self, level):
 		keyboard = Keyboard()
 
 		# IDLE
-		if self.state == "idle":
+		if self.state["idle"]:
 			# MOVING LEFT
 			if keyboard.key_pressed("left") and keyboard.key_pressed("right") == False:
 				self.set_direction("left")
@@ -70,7 +88,7 @@ class Entity(object):
 				self.set_state("jumping")
 
 		# MOVING
-		elif self.state == "walking":
+		elif self.state["walking"]:
 			# STOPPED MOVING
 			if (keyboard.key_pressed("left") == False and keyboard.key_pressed("right") == False or 
 				keyboard.key_pressed("left") and keyboard.key_pressed("right")):
@@ -78,45 +96,38 @@ class Entity(object):
 			# JUMPING
 			elif keyboard.key_pressed("up"):
 				self.set_state("jumping")
-			#TODO COLLISION
-			for obstacle in level.obstacles:
-				if self.animation.collided(obstacle):
-					self.set_state("idle")
 
 		# JUMPING
-		elif self.state == "jumping":
+		elif self.state["jumping"]:
 			# MOVING LEFT
 			if keyboard.key_pressed("left") and keyboard.key_pressed("right") == False:
 				self.set_direction("left")
 			# MOVING RIGHT
 			elif keyboard.key_pressed("right") and keyboard.key_pressed("left") == False:
 				self.set_direction("right")
+		
+		# COLLINDING
+		elif self.state["colliding"]:
+			if self.direction["left"] and keyboard.key_pressed("right"):
+				pass
+			elif self.direction["right"] and keyboard.key_pressed("left"):
+				pass
 
 	def set_state(self, state):
-		if state == "idle" or state == "walking" or state == "jumping":
-			self.state = state
-		else:
-			self.state = "idle"
-			print("error: set_state entity")
+		for key, _ in self.state.items():
+			if key == state:
+				self.state[key] = True
+			else:
+				self.state[key] = False
 
 	def set_direction(self, direction):
-		if direction == "right" or direction == "left":
-			self.direction = direction
-		else:
-			self.direction = "right"
-			print("error: set_direction entity")
+		for key, _ in self.direction.items():
+			if key == direction:
+				self.direction[key] = True
+			else:
+				self.direction[key] = False
 
 	def set_animation(self, sprite_path, frames):
 		self.animation.image, self.animation.rect = load_image(sprite_path, alpha=True)
 		self.animation.set_total_duration(frames * globals.FRAME_SPEED)
-	
-	def walk(self, dx):
-		self.animation.x += dx
-
-	def jump(self, dy, dt):
-		self.animation.y = self.y0 - dy * self.y_time - (globals.GRAVITY * (self.y_time)**2)/2
-		self.y_time += globals.FALL_TIME * dt
-	
-	def attack(self):
-		pass
 	
