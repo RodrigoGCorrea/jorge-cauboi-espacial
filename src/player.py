6647a35f716de0a5f53ea180f0008cfb2f523828
Geraldo Fada <geraldofada.neto@gmail.com>
2019-06-14 01:41:16 -0300

    new module to handle the player using the entity class

--- src/player.py
+++ src/player.py
@@ -1,37 +1,57 @@
-from pygame.transform import flip
+from pygame.math import Vector2
+
+from window import window
 from entity import Entity
 from library.PPlay.keyboard import Keyboard
+
 import globals
 
 keyboard = Keyboard()
+player = Entity(window, "./src/assets/actors/jorge/idle_right.png", 8)
+player.set_position(10, window.height / 2)
+
+vel_stop = Vector2(0)
+vel_right = Vector2(globals.VELOCITY_PLAYER, 0)
+vel_left = Vector2(-globals.VELOCITY_PLAYER, 0)
+vel_down = Vector2(0, globals.VELOCITY_PLAYER)
+vel_up = Vector2(0, -globals.VELOCITY_PLAYER)
 
 
-class Player(Entity):
-    def __init__(self, window, animation_path, animation_frame):
-        super().__init__(window, animation_path, animation_frame)
-        self.animation.set_position(globals.WIDTH / 2, 500)
+def run():
+    if (
+        keyboard.key_pressed("right") == False
+        and keyboard.key_pressed("left") == False
+        and keyboard.key_pressed("up") == False
+        and keyboard.key_pressed("down") == False
+    ):
+        player.move(vel_stop)
+        if player.direction["right"]:
+            player.set_animation("./src/assets/actors/jorge/idle_right.png", 8)
+        elif player.direction["left"]:
+            player.set_animation("./src/assets/actors/jorge/idle_left.png", 8)
 
-    def update(self, level):
-        self.clear_variables()
-        self.fall()
-        self.walk()
-        self.jump()
-        self.collision_y(level)
-        self.collision_x(level)
+    if keyboard.key_pressed("right"):
+        player.move(vel_right)
+        player.set_animation("./src/assets/actors/jorge/running_right.png", 8)
+        player.flip_direction()
 
-    def render(self):
-        if (
-            self.velocity["x"] == 0
-            and self.velocity["y"] == 0
-            and globals.CAMERA_MOVING == False
-            or self.jumping
-        ):
-            self.set_animation("./src/assets/actors/jorge/idle.png", 8)
-        else:
-            self.set_animation("./src/assets/actors/jorge/running.png", 8)
+    elif keyboard.key_pressed("left"):
+        player.move(vel_left)
+        player.set_animation("./src/assets/actors/jorge/running_left.png", 8)
+        player.flip_direction()
 
-        if self.direction["left"]:
-            self.animation.image = flip(self.animation.image, True, False)
+    if keyboard.key_pressed("down"):
+        player.move(vel_down)
+        if player.direction["right"]:
+            player.set_animation("./src/assets/actors/jorge/running_right.png", 8)
+        elif player.direction["left"]:
+            player.set_animation("./src/assets/actors/jorge/running_left.png", 8)
+    elif keyboard.key_pressed("up"):
+        player.move(vel_up)
+        if player.direction["right"]:
+            player.set_animation("./src/assets/actors/jorge/running_right.png", 8)
+        elif player.direction["left"]:
+            player.set_animation("./src/assets/actors/jorge/running_left.png", 8)
 
-        self.animation.update()
-        self.animation.draw()
+    player.update()
+    player.render()
