aada95ed4fd96625fed02fa331ba0718839a4542
Geraldo Fada <geraldofada.neto@gmail.com>
2019-06-14 13:04:53 -0300

    keyboard and window are in a new module

--- src/instances.py
+++ src/instances.py
@@ -1,6 +1,9 @@
 from library.PPlay.window import Window
+from library.PPlay.keyboard import Keyboard
 
 import globals
+
+keyboard = Keyboard()
 
 window = Window(globals.WIDTH, globals.HEIGHT)
 window.set_title("JSC 0.1.0 alpha")
