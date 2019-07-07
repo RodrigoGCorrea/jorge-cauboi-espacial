f7516b2c9e441479830951d7da57c72de4877a12
Geraldo Fada <geraldofada.neto@gmail.com>
2019-07-07 12:14:47 -0300

    new store instance

--- src/environment/instances.py
+++ src/environment/instances.py
@@ -2,6 +2,8 @@ from library.PPlay.window import Window
 from library.PPlay.keyboard import Keyboard
 from library.PPlay.mouse import Mouse
 
+from classes.store import Store
+
 from . import variables as gvar
 
 window = Window(gvar.WIDTH, gvar.HEIGHT)
@@ -10,3 +12,5 @@ window.set_title("JSC 1.0.0")
 keyboard = Keyboard()
 
 mouse = Mouse()
+
+store = Store()
