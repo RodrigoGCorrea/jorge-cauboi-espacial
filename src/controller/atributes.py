6e56b57d0a0327235320110ec2d90ca9bae6c523
Geraldo Fada <geraldofada.neto@gmail.com>
2019-07-07 12:50:12 -0300

    attributes connected with store

--- src/controller/atributes.py
+++ src/controller/atributes.py
@@ -1,49 +1,46 @@
 from math import exp
+from environment.instances import store
 
-owned_strenght = 0
+owned_strength = 0
 owned_life = 0
 owned_heal = 0
 
 
 def reset():
-    global owned_strenght
+    global owned_strength
     global owned_life
     global owned_heal
 
-    owned_strenght = 0
+    owned_strength = 0
     owned_life = 0
     owned_heal = 0
 
 
 # STRENGHT
 def cost_strength():
-    global owned_strenght
-    return int(100 * (1.3) ** owned_strenght)
+    return int(100 * (1.3) ** store.get("owned_strength"))
 
 
-def update_strenght():
-    global owned_strenght
-    owned_strenght += 1
-    return int(50 * exp(0.1255 * owned_strenght))
+def update_strength():
+    store.dispatch("owned_strength", lambda strength: strength + 1)
+    return int(50 * exp(0.1255 * store.get("owned_strength")))
 
 
 # LIFE
 def cost_life():
-    return int(100 * (1.4) ** owned_life)
+    return int(100 * (1.4) ** store.get("owned_life"))
 
 
 def update_life():
-    global owned_life
-    owned_life += 1
+    store.dispatch("owned_life", lambda life: life + 1)
     return 10
 
 
 # HEAL
 def cost_heal():
-    return int(100 * (1.5) ** owned_heal)
+    return int(100 * (1.5) ** store.get("owned_heal"))
 
 
 def heal_player(player):
-    global owned_heal
-    owned_heal += 1
+    store.dispatch("owned_heal", lambda heal: heal + 1)
     player.life = player.max_life
