8733846346bb6d5efd4b0b36f20aa23cdca1e261
Geraldo Fada <geraldofada.neto@gmail.com>
2019-07-07 12:08:36 -0300

    new config file for const variables and new store class

---
+++ src/classes/store.py
@@ -0,0 +1,24 @@
+class Store(object):
+    def __init__(self, initial_store=None):
+        if initial_store != None and type(initial_store) is dict:
+            self.store = initial_store
+        else:
+            self.store = {
+                "game_started": True,
+                "state": 0,
+                "score": 0,
+                "enemy_mtx": [],
+                "player": None,
+                "owned_strength": 0,
+                "owned_life": 0,
+                "owned_heal": 0
+            }
+
+    def dispatch(self, name, value=0, callback=None):
+        if callback != None and callable(callback):
+            callback(self.store[name])
+        else:
+            self.store[name] = value
+
+    def get(self, name):
+        return self.store[name]
